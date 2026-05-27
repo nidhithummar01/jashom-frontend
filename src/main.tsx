8c808271780376a070c1540a90c12af5c319d03f
nidhithummar01 <nidhi.thummar@jashom.com>
2026-05-27 15:15:05 +0530

    lyro remove and new aichat bot integrate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
-import { loadTidio } from './lib/loadTidio'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,16 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </HelmetProvider>
 )
-
-function scheduleTidio(): void {
-  const run = () => {
-    loadTidio()
-  }
-  if (typeof window !== 'undefined' && 'requestIdleCallback' in window) {
-    window.requestIdleCallback(run, { timeout: 2500 })
-  } else {
-    setTimeout(run, 500)
-  }
-}
-scheduleTidio()
 
